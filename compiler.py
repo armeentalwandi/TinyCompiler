27b74eab95aeffc533f759d3b0c0283afdc5394f
Armeen Talwandi <armeentalwandi@gmail.com>
2024-09-30 21:49:32 -0400

    started parser

--- compiler.py
+++ compiler.py
@@ -6,9 +6,8 @@ def main():
   if len(sys.argv) != 2:
     sys.exit("argument number is incorrect")
   
-  source_file = open(sys.argv[1], 'r')
-  source = source_file.read()
-
+  with open(sys.argv[1], 'r') as source_file: #opens (and takes care of closing the entire file - source_file is the variable assigned to the opened file)
+    source = source_file.read() #reads the entire file and stores in source
 
   lexer = Lexer(source)
   parser = Parse(lexer)
--- parse.py
+++ parse.py
@@ -1,3 +1,32 @@
+import sys
+from lexer import *
 
+# parse object is gonna keep track of current and previous tokens and make sure they match the grammar
 class Parse:
-  def __init__(self, source)
+  def __init__(self, lexer):
+    pass
+
+  # returns true if the current token matches the kind of the grammar
+  def checkToken(self, kind):
+    pass
+
+  # returns true if the next token matches the kind of the grammar
+  def checkPeek(self, kind):
+    pass
+
+  # tries to match the current token. if not an error, moves on to the text token
+  def match(self, kind):
+    pass
+
+  # advances to the next token
+  def nextToken(self):
+    pass
+
+  # error message
+  def abort(self, message):
+    sys.exit("Error!: " + message)
+
+
+
+
+
