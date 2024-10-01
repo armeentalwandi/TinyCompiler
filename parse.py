54ced1ae95dc34acb48f25c1d34ce5ff71fe96a1
Armeen Talwandi <armeentalwandi@gmail.com>
2024-09-30 22:08:23 -0400

    set up basic functionality for grammar checking

--- parse.py
+++ parse.py
@@ -4,23 +4,33 @@ from lexer import *
 # parse object is gonna keep track of current and previous tokens and make sure they match the grammar
 class Parse:
   def __init__(self, lexer):
-    pass
+    self.lexer = lexer
+    self.curToken = None
+    self.peekToken = None
+    self.nextToken()
+    self.nextToken() # initalizes peek and curr token properly!
+
 
   # returns true if the current token matches the kind of the grammar
   def checkToken(self, kind):
-    pass
+    return self.curToken.kind == kind
 
   # returns true if the next token matches the kind of the grammar
   def checkPeek(self, kind):
-    pass
+    return self.peekToken.kind == kind
 
   # tries to match the current token. if not an error, moves on to the text token
   def match(self, kind):
-    pass
+    if self.checkToken(kind):
+      self.nextToken()
+    else:
+      self.abort("token does not match. Expected: " + kind.name + ". Recieved: " + self.curToken.kind.name)
+    
 
   # advances to the next token
   def nextToken(self):
-    pass
+    self.curToken = self.peekToken
+    self.peekToken = self.lexer.getToken() # get token gets the next token! 
 
   # error message
   def abort(self, message):
